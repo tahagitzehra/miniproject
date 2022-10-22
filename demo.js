ab913df538e715f367cdad1bcf2837b2d18978ca
Taha Zehra <tahazehra7@gmail.com>
2022-10-21 22:17:52 -0700

    Initial commit

---
+++ demo.js
@@ -0,0 +1,32 @@
+document.getElementById("hidec").onclick = function() {
+  
+    document.getElementById("circle").style.display = "none";
+
+}
+
+document.getElementById("hider").onclick = function() {
+
+    document.getElementById("rounded").style.display = "none";
+
+}
+
+document.getElementById("hides").onclick = function() {
+
+    document.getElementById("square").style.display = "none";
+
+}
+document.getElementById("showc").onclick = function() {
+
+    document.getElementById("circle").style.display = "block";
+
+}
+document.getElementById("showr").onclick = function() {
+
+    document.getElementById("rounded").style.display = "block";
+    
+}
+document.getElementById("shows").onclick = function() {
+
+    document.getElementById("square").style.display = "block";
+    
+}
